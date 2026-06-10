ea8e4de12660a7fa09fe74ec2861882c36825b5d
yassineelkharmoudi <yassine.elkharmoudi.cons@autorita-trasporti.it>
2026-06-10 14:38:13 +0200

    deploy

--- src/App.jsx
+++ src/App.jsx
@@ -5,22 +5,36 @@ function App() {
   const [count, setCount] = useState(0);
 const name = "Yassine";
 const cognome = "El Kharmoudi";
+const users = [
+  { id: 1, name: "Mario" },
+  { id: 2, name: "Luca" },
+  { id: 3, name: "Anna" }
+];
 
 const userDetail = {
   name,
   cognome
 };
 
-const user = {
-  ...userDetail,
-  //cognome:"Tomaso"
-};
+// const user = {
+//   ...userDetail,
+//   //cognome:"Tomaso"
+// };
 
 return (
   <div>
     <h3>
       Benvenuto nella mia pagina Web di: {user.name} - {user.cognome}
     </h3>
+    <ul>
+      {
+        users.map(x=>(
+      <li key={x.id}>
+        {x.name}
+      </li>
+     ))
+    }
+    </ul>
   </div>
 );
 
