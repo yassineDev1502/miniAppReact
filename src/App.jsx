4429cfa81f26ad0d874a2c117d1a0eb9a4d1ac65
yassineelkharmoudi <yassine.elkharmoudi.cons@autorita-trasporti.it>
2026-06-10 12:47:46 +0200

    secondo commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,38 @@ import { useState } from "react";
 
 function App() {
   const [count, setCount] = useState(0);
+const name = "Yassine";
+const cognome = "El Kharmoudi";
 
-  return (
-    <div>
-      <h1>Lista Numweri</h1>
+const userDetail = {
+  name,
+  cognome
+};
 
-      <p>Count: {count}</p>
+const user = {
+  ...userDetail,
+  //cognome:"Tomaso"
+};
 
-      <button onClick={() => setCount(count + 1)}>
-        Incrementa
-      </button>
-    </div>
-  );
+return (
+  <div>
+    <h3>
+      Benvenuto nella mia pagina Web di: {user.name} - {user.cognome}
+    </h3>
+  </div>
+);
+
+  // return (
+  //   <div>
+  //     <h3>benvenuto nella mia pagina Web di : {user.name}{user.cognome} </h3>
+
+  //     <p>Count: {count}</p>
+
+  //     <button onClick={() => setCount(count + 1)}>
+  //       Incrementa
+  //     </button>
+  //   </div>
+  // );
 }
 
 export default App;
